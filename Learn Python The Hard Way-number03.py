fec296755f8ba8790cf0e58d5e7fa78609f42ec8
xingshu1990 <13587678633@139.com>
2016-05-08 17:33:32 +0800

    Create Learn Python The Hard Way-number03.py

---
+++ Learn Python The Hard Way-number03.py
@@ -0,0 +1,44 @@
+# -- coding: utf-8 --
+
+print "I will now count my chickens:"
+#上面打印文字就不说了。
+print "Hens",25+30/6
+#这里使用+ /进行计算  中间的,作用这里也没说，后面有讲到（其实忘记了233）
+print "Roosters",100-25*3%4
+
+print "Now I will count the eggs:"
+print 3 +2 +1 -5 +4%2-1/4+6
+
+print "Is it true that 3 +2 <5-7?"
+print 3+2<5-7
+
+print "What is 3+2?", 3+2
+print "What is 5-7?", 5-7
+
+print "Oh,that's why it's False."
+print "How about some more."
+
+print "Is it greater?",5>-2
+print "Is it greater or equal?",5>= -2
+print "Is it less or equal?",5<= -2
+
+
+使用 # 在代码每一行的前一行为自己写一个注解，说明一下这一行的作用。
+————英文不好啊
+
+
+记得开始时的 <练习 0> 吧？用里边的方法把 Python 运行起来，然后使用刚才学到的运算符号，把Python当做计算器玩玩。
+自己找个想要计算的东西，写一个 .py 文件把它计算出来。
+有没有发现计算结果是”错”的呢？计算结果只有整数，没有小数部分。研究一下这是为什么，搜索一下“浮点数(floating point number)”是什么东西。
+使用浮点数重写一遍 ex3.py，让它的计算结果更准确(提示: 20.0 是一个浮点数)。
+
+其他注释就不写了，等用到的时候再说
+浮点数也不写了
+
+
+
+
++  #有计算和字符串拼接的作用
+- #比较多的是他的数学计算里的相减
+/
+*
