6b9e84c83fef2f70cf3408e85b333347ebe2410d
xingshu1990 <13587678633@139.com>
2016-05-11 22:22:48 +0800

    Create number09-Learn Python The Hard Way.py

---
+++ number09-Learn Python The Hard Way.py
@@ -0,0 +1,19 @@
+# -- coding: utf-8 --
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+#星期
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#\n换行
+
+print "Here are the days:",days
+#前面字符串，后面是days对应的内容
+print "Here are the months",months
+
+print '''
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+'''
+
+这章节基本上没问题
