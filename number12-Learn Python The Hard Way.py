517bdabc98e51aae14ae99fed721bb565837b45c
xingshu1990 <13587678633@139.com>
2016-05-14 12:12:47 +0800

    Update number12-Learn Python The Hard Way.py

--- number12-Learn Python The Hard Way.py
+++ number12-Learn Python The Hard Way.py
@@ -2,3 +2,11 @@
 
 y = raw_input("age?")
 print y 
+
+#上面这样raw_input("内容") 其实已经可以了
+
+age = raw_input("how old are you?")
+height = raw_input("how tall are you?")
+weight = raw_input("how mach do you weight?")
+
+print "%s %s %s" %(age,height,weight) #以自己的方式来写格式化
