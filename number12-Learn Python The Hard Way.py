07d0f32ccad840626398a3ad7600ae63f0eefc3d
xingshu1990 <13587678633@139.com>
2016-05-15 19:09:14 +0800

    Update number12-Learn Python The Hard Way.py

--- number12-Learn Python The Hard Way.py
+++ number12-Learn Python The Hard Way.py
@@ -10,3 +10,11 @@ height = raw_input("how tall are you?")
 weight = raw_input("how mach do you weight?")
 
 print "%s %s %s" %(age,height,weight) #以自己的方式来写格式化
+
+
+
+习题：
+1.直接在命令窗口下打入代码就可以看到内容了
+2.嗯
+3.忘记了
+4.看不懂英文
